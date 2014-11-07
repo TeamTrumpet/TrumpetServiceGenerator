54dc033fa6c65a3a870854cbb72ffbbbfc538748
Wyatt Johnson <wyattjoh@gmail.com>
2014-11-07 12:36:16 -0700

    Added base service

--- lib/generators/trumpet_service/trumpet_service_generator.rb
+++ lib/generators/trumpet_service/trumpet_service_generator.rb
@@ -3,6 +3,7 @@ class TrumpetServiceGenerator < Rails::Generators::NamedBase
   argument :service_module, :type => :string
 
   def generate_service
+    template "service_base.rb", "app/services/service.rb"
     template "service.rb", "app/services/#{service_module.underscore}_service/#{name.underscore}.rb"
     template "rspec.rb", "spec/services/#{service_module.underscore}_service/#{name.underscore}_spec.rb"
   end
--- trumpet_service_generator.gemspec
+++ trumpet_service_generator.gemspec
@@ -3,7 +3,7 @@ $:.push File.expand_path("../lib", __FILE__)
 # Describe your gem and declare its dependencies:
 Gem::Specification.new do |s|
   s.name        = "trumpet_service_generator"
-  s.version     = 1.0
+  s.version     = 1.1
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Wyatt Johnson"]
   s.email       = ["wyattjoh@gmail.com"]
